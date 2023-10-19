d1785cc91f46abd4a529b0b3365053df78ad74a5
ASN11 <andre92.91@bk.ru>
2023-10-19 17:35:12 +0300

    final

---
+++ src/main/java/ru/testsber/TestSberApplication.java
@@ -0,0 +1,13 @@
+package ru.testsber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSberApplication.class, args);
+    }
+
+}
